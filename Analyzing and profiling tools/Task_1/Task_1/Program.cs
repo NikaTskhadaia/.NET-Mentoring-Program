b57cff6223ea89458a88f73c402ca5298d01d97b
Nikoloz Tskhadaia <Nikoloz_Tskhadaia@epam.com>
2022-11-09 10:17:54 +0400

    change key derivation algorithm

--- Analyzing and profiling tools/Task_1/Task_1/Program.cs
+++ Analyzing and profiling tools/Task_1/Task_1/Program.cs
@@ -1,16 +1,21 @@
 ﻿using System.Diagnostics;
 using System.Security.Cryptography;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+
+object obj = new();
+
+GeneratePasswordUsingKeyDerivation("nikoloz", new byte[16]);
 
 Stopwatch stopwatch = new();
 stopwatch.Start();
-var improvedHash = GeneratePasswordMultipleGetBytes("nikoloz", new byte[16]);
+var hashedKeyDerivation = GeneratePasswordUsingKeyDerivation("nikoloz", new byte[16]);
 stopwatch.Stop();
-Console.WriteLine($"{stopwatch.ElapsedTicks} - {improvedHash} - improved");
+Console.WriteLine($"- {stopwatch.ElapsedTicks} {hashedKeyDerivation} - using Microsoft.AspNetCore.Cryptography.KeyDerivation");
 stopwatch.Reset();
 stopwatch.Start();
 var hashedPassword = GeneratePasswordHashUsingSalt("nikoloz", new byte[16]);
 stopwatch.Stop();
-Console.WriteLine($"{stopwatch.ElapsedTicks} - {improvedHash}");
+Console.WriteLine($"- {stopwatch.ElapsedTicks} {hashedPassword} - using System.Security.Cryptography.Rfc2898DeriveBytes");
 
 Console.ReadLine();
 
@@ -28,18 +33,13 @@ string GeneratePasswordHashUsingSalt(string passwordText, byte[] salt)
     return passwordHash;
 }
 
-string GeneratePasswordMultipleGetBytes(string passwordText, byte[] salt)
+string GeneratePasswordUsingKeyDerivation(string passwordText, byte[] salt)
 {
-    var iterate = 10000;
-    var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
-    List<byte> hash = new(20);
-    for (int i = 0; i < 20; i++)
-    {
-        hash.Add(pbkdf2.GetBytes(1)[0]);
-    }
+    var hash = KeyDerivation.Pbkdf2(passwordText, salt, KeyDerivationPrf.HMACSHA1, 10000, 20);
+
     byte[] hashBytes = new byte[36];
     Array.Copy(salt, 0, hashBytes, 0, 16);
-    Array.Copy(hash.ToArray(), 0, hashBytes, 16, 20);
+    Array.Copy(hash, 0, hashBytes, 16, 20);
 
     var passwordHash = Convert.ToBase64String(hashBytes);
 
