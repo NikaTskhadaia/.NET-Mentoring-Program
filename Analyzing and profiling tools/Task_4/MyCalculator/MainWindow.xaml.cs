b75f8d293e68148debceafddc87e5b059f2ad693
Nikoloz Tskhadaia <Nikoloz_Tskhadaia@epam.com>
2022-11-13 01:21:25 +0400

    resolve exception in calculator

--- Analyzing and profiling tools/Task_4/MyCalculator/MainWindow.xaml.cs
+++ Analyzing and profiling tools/Task_4/MyCalculator/MainWindow.xaml.cs
@@ -5,6 +5,7 @@
 // Assembly location: C:\Users\Nikoloz_Tskhadaia\Downloads\DumpHomework\DumpHomework\MyCalculator.exe
 
 using System;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
@@ -12,59 +13,64 @@ using System.Windows.Markup;
 namespace MyCalculatorv1
 {
     public partial class MainWindow : Window, IComponentConnector
-  {
-
-    public MainWindow() => this.InitializeComponent();
-
-    private void Button_Click_1(object sender, RoutedEventArgs e) => this.tb.Text += ((Button) sender).Content.ToString();
-
-    private void Result_click(object sender, RoutedEventArgs e) => this.result();
-
-    private void result()
     {
-      int num1 = 0;
-      if (this.tb.Text.Contains("+"))
-        num1 = this.tb.Text.IndexOf("+");
-      else if (this.tb.Text.Contains("-"))
-        num1 = this.tb.Text.IndexOf("-");
-      else if (this.tb.Text.Contains("*"))
-        num1 = this.tb.Text.IndexOf("*");
-      else if (this.tb.Text.Contains("/"))
-        num1 = this.tb.Text.IndexOf("/");
-      string str = this.tb.Text.Substring(num1, 1);
-      double num2 = Convert.ToDouble(this.tb.Text.Substring(0, num1));
-      double num3 = Convert.ToDouble(this.tb.Text.Substring(num1 + 1, this.tb.Text.Length - num1 - 1));
-      if (str == "+")
-      {
-        TextBox tb = this.tb;
-        tb.Text = tb.Text + "=" + (object) (num2 + num3);
-      }
-      else if (str == "-")
-      {
-        TextBox tb = this.tb;
-        tb.Text = tb.Text + "=" + (object) (num2 - num3);
-      }
-      else if (str == "*")
-      {
-        TextBox tb = this.tb;
-        tb.Text = tb.Text + "=" + (object) (num2 * num3);
-      }
-      else
-      {
-        TextBox tb = this.tb;
-        tb.Text = tb.Text + "=" + (object) (num2 / num3);
-      }
+
+        public MainWindow() => this.InitializeComponent();
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) => this.tb.Text += ((Button)sender).Content.ToString();
+
+        private void Result_click(object sender, RoutedEventArgs e) => this.result();
+
+        private void result()
+        {
+            int operationIndex = 0;
+            if (this.tb.Text.Contains("+"))
+                operationIndex = this.tb.Text.IndexOf("+");
+            else if (this.tb.Text.Contains("-"))
+                operationIndex = this.tb.Text.IndexOf("-");
+            else if (this.tb.Text.Contains("*"))
+                operationIndex = this.tb.Text.IndexOf("*");
+            else if (this.tb.Text.Contains("/"))
+                operationIndex = this.tb.Text.IndexOf("/");
+            string operation = this.tb.Text.Substring(operationIndex, 1);
+            double num1 = Convert.ToDouble(this.tb.Text.Substring(0, operationIndex));
+            int endOfFirstNumberIndex = Regex.Match(this.tb.Text.Substring(operationIndex + 1, this.tb.Text.Length - operationIndex - 1), @"[+-/*=]").Index;
+            if (endOfFirstNumberIndex == 0)
+            {
+                endOfFirstNumberIndex = this.tb.Text.Length - operationIndex - 1;
+            }
+            double num2 = Convert.ToDouble(this.tb.Text.Substring(operationIndex + 1, endOfFirstNumberIndex));
+            if (operation == "+")
+            {
+                TextBox tb = this.tb;
+                tb.Text = tb.Text + "=" + (object)(num1 + num2);
+            }
+            else if (operation == "-")
+            {
+                TextBox tb = this.tb;
+                tb.Text = tb.Text + "=" + (object)(num1 - num2);
+            }
+            else if (operation == "*")
+            {
+                TextBox tb = this.tb;
+                tb.Text = tb.Text + "=" + (object)(num1 * num2);
+            }
+            else
+            {
+                TextBox tb = this.tb;
+                tb.Text = tb.Text + "=" + (object)(num1 / num2);
+            }
+        }
+
+        private void Off_Click_1(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
+
+        private void Del_Click(object sender, RoutedEventArgs e) => this.tb.Text = "";
+
+        private void R_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.tb.Text.Length <= 0)
+                return;
+            this.tb.Text = this.tb.Text.Substring(0, this.tb.Text.Length - 1);
+        }
     }
-
-    private void Off_Click_1(object sender, RoutedEventArgs e) => Application.Current.Shutdown();
-
-    private void Del_Click(object sender, RoutedEventArgs e) => this.tb.Text = "";
-
-    private void R_Click(object sender, RoutedEventArgs e)
-    {
-      if (this.tb.Text.Length <= 0)
-        return;
-      this.tb.Text = this.tb.Text.Substring(0, this.tb.Text.Length - 1);
-    }
-  }
 }
