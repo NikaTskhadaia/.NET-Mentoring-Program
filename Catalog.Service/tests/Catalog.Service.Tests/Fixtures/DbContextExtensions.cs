893384f2eaf95e4ed0bfc0084e2c25524029436e
Nikoloz Tskhadaia <Nikoloz_Tskhadaia@epam.com>
2022-11-18 16:32:12 +0400

    refactor

--- Catalog.Service/tests/Catalog.Service.Tests/Fixtures/DbContextExtensions.cs
+++ Catalog.Service/tests/Catalog.Service.Tests/Fixtures/DbContextExtensions.cs
@@ -9,11 +9,7 @@ internal static class DbContextExtensions
     public static IServiceCollection AddDbContextOptions<TContext>(this IServiceCollection services, Action<DbContextOptionsBuilder<TContext>> configure) where TContext : DbContext
     {
         // Remove the existing DbContextOptions
-        // we want to override the settings and calling AddDbContext<TContext> again
-        // will noop.
         services.RemoveAll(typeof(DbContextOptions<TContext>));
-
-        // Add the options as singletons since the IDbContextFactory as a singleton
 
         // Add the DbContextOptions<TContext>
         services.AddSingleton(s =>
